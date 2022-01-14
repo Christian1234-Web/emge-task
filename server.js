700ebd0b7dcde2012ebfb9d367f8c43558dc097a
christian <ebukaugwulast@gmail.com>
2022-01-14 20:30:42 +0100

    edited sever,js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(cors());
 app.use(routes);
 
 if (process.env.NODE_ENV == 'production') {
-	app.use(express.static('ui/build'));
+	app.use(express.static('client/build'));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "ui", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	})
 }
 app.listen(port);
